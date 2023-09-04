00922fd14ad91d1854f33b7feea802a7fea0342b
Kobi Feldman <kobif2001@gmail.com>
2023-09-04 13:16:30 -0400

    update to draw dotted lines instead of solid

--- draw.py
+++ draw.py
@@ -14,11 +14,22 @@ def draw(output_path: str, img_path: str, bound_list: list):
 def draw_rectangle(image, bounds: list):
     """Draw a yellow dotted rectangle onto a given image at the specified coordinates."""
     
-    start_point = (bounds[0], bounds[1])
-    end_point = (bounds[2], bounds[3])
+    x1, y1 = bounds[0], bounds[1]
+    x2, y2 = bounds[2], bounds[3]
     color = (64, 231, 241)
-    thickness = 3
+    thickness = 6
+    line_type = cv2.LINE_8
+    dash_length = 10
 
-    image = cv2.rectangle(image, start_point, end_point, color, thickness)
+    # **These two for loops were generated using ChatGPT**
+    # Draw horizontal dashes
+    for x in range(x1, x2, dash_length * 2):
+        image = cv2.line(image, (x, y1), (min(x + dash_length, x2), y1), color, thickness, line_type)
+        image = cv2.line(image, (x, y2), (min(x + dash_length, x2), y2), color, thickness, line_type)
+
+    # Draw vertical dashes
+    for y in range(y1, y2, dash_length * 2):
+        image = cv2.line(image, (x1, y), (x1, min(y + dash_length, y2)), color, thickness, line_type)
+        image = cv2.line(image, (x2, y), (x2, min(y + dash_length, y2)), color, thickness, line_type)
 
     return image
